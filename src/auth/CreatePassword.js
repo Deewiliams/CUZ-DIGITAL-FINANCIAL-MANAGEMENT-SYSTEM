d4580020e0556930b7a5d69d2c8276e930697b9e
Desire <desireirankundawilliams@gmail.com>
2025-11-05 23:50:12 +0200

    cleaned the code

--- src/auth/CreatePassword.js
+++ src/auth/CreatePassword.js
@@ -1,16 +1,50 @@
-import { Container, PasswordInput, Stack } from "@mantine/core";
+import { Container, PasswordInput, Stack, Checkbox, Text } from "@mantine/core";
 import React from "react";
 
-const CreatePassword = () => {
+const CreatePassword = ({ form }) => {
   return (
     <Container size={600}>
       <Stack gap="sm">
-        <PasswordInput label="Password" placeholder="*********" size="md" />
-      <PasswordInput
-        label="Comfirm Password"
-        placeholder="*********"
-        size="md"
-      />
+        <PasswordInput
+          label="Password"
+          placeholder="*********"
+          size="md"
+          required
+          {...form.getInputProps("password")}
+        />
+
+        <PasswordInput
+          label="Confirm Password"
+          placeholder="*********"
+          size="md"
+          required
+          {...form.getInputProps("confirmPassword")}
+        />
+
+        <Checkbox
+          mt="md"
+          label={
+            <Text size="sm">
+              I accept the{" "}
+              <Text
+                component="span"
+                c="blue"
+                style={{ textDecoration: "underline", cursor: "pointer" }}
+              >
+                Terms of Service
+              </Text>{" "}
+              and{" "}
+              <Text
+                component="span"
+                c="blue"
+                style={{ textDecoration: "underline", cursor: "pointer" }}
+              >
+                Privacy Policy
+              </Text>
+            </Text>
+          }
+          {...form.getInputProps("termsOfService", { type: "checkbox" })}
+        />
       </Stack>
     </Container>
   );
