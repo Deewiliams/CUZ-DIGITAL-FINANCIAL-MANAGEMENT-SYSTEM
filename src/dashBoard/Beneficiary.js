3763e10d836c7a2fe4af9491b61887bf8c3a24d0
Samson Kwizela <kizelasamson@gmail.com>
2025-11-11 21:37:56 -0800

    modified bebeficiary

--- src/dashBoard/Beneficiary.js
+++ src/dashBoard/Beneficiary.js
@@ -1,31 +1,77 @@
 import React from "react";
-import { Container, Input } from "@mantine/core";
-import { TextInput } from "@mantine/core";
-import { Textarea } from "@mantine/core";
+import { Container, Input, TextInput, Textarea, Title, Card, Button, Group } from "@mantine/core";
 
 const Beneficiary = () => {
   return (
-    <div>
-      <Container>
-      <Input.Wrapper
-        label="Account Number"
-        description="STU-77565457"
-        error="Input error"
+    <Container size="sm" py="xl">
+      <Card
+        shadow="md"
+        radius="lg"
+        withBorder
+        p="lg"
+        style={{
+          backgroundColor: "var(--mantine-color-gray-0)",
+          borderRadius: "20px",
+        }}
       >
-        <Input placeholder="Input inside Input.Wrapper" />
-      </Input.Wrapper>
-      <TextInput
-        label="Nick Name"
-        description=""
-        placeholder="Cavendish FAO"
-      />
-      <Textarea
-        label="Description"
-        description="Input description"
-        placeholder="He is ....."
-      />
-      </Container>
-    </div>
+        <Title
+          order={3}
+          align="center"
+          style={{
+            marginBottom: "20px",
+            color: "var(--mantine-color-blue-6)",
+            fontWeight: 600,
+          }}
+        >
+          Beneficiary Details
+        </Title>
+
+        <Input.Wrapper
+          label="Account Number"
+          description="STU-77565457"
+          withAsterisk
+          mb="md"
+        >
+          <Input
+            placeholder="Enter beneficiary account number"
+            radius="md"
+            size="md"
+          />
+        </Input.Wrapper>
+
+        <TextInput
+          label="Nick Name"
+          placeholder="Cavendish FAO"
+          radius="md"
+          size="md"
+          mb="md"
+        />
+
+        <Textarea
+          label="Description"
+          placeholder="He is..."
+          radius="md"
+          size="md"
+          autosize
+          minRows={3}
+          mb="lg"
+        />
+
+        <Group position="center" mt="md">
+          <Button
+            radius="md"
+            size="md"
+            color="blue"
+            style={{
+              paddingLeft: "30px",
+              paddingRight: "30px",
+            }}
+          >
+            Save Beneficiary
+          </Button>
+        </Group>
+      </Card>
+    </Container>
   );
 };
 
